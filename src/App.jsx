ee41ec88c18228455760da23aa948da6433b8dc7
Victor87dev <devsjoaovvictor@gmail.com>
2023-09-09 10:45:53 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import Logo from './componentes/imgs/naruto.png'
 
 export default function App(){
+  const saitama=()=>{
+    return('cfb curso')
+  }
+  function matematico(){
+    return 'alow'
+  }
   return(
-  <>
-  <h1>Iniciando React</h1>
-  <p>CFB cursos</p>
-  <br/>
-  <p>Curso de react</p>
-  <p>app</p>
-  </>
+  <section>
+    <header>
+      <h1>Iniciando React</h1>
+      <p>CFB cursos {saitama()}</p>
+      <br/>
+      <p>Curso de react {matematico()}</p>
+      <p>app</p>
+    </header>
+     
+    <section>
+      <p>pq devo vencer ?, pq sou o melhor e sempre serei o melhor. MESTRE SAITAMA</p>
+      <img src={Logo}/>
+      <img src='/img/saitama.png'/>
+    </section>
+ 
+  </section>
   )
 }
 
