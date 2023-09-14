91ef04501838f0910e3d5a8d2d3c793e8dabcb24
Victor87dev <devsjoaovvictor@gmail.com>
2023-09-14 11:35:56 -0300

    ususeState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './componentes/Header'
 import Corpo from './componentes/Corpo'
 import './App.css'
-
+import Relogio from './componentes/Relogio'
+import Numero from './componentes/Numero'
 
 export default function App(){
+const [num,setNum] = useState(20)
+const [nome,setNome] = useState('joao')
+let n1 = 10
+
+const n100 = ()=>{
+n1 = 100
+console.log(n1)
+}
+
 
   return(
   <>
+  
   <p className='texto'>heloow</p>
   <Header/>
   <Corpo/>
+  <Relogio/>
+  <p>Valor do state num em App: {num}</p>
+  <Numero num={num} setNum={setNum}/>
+  <p>{nome}</p>
   </>
+  
   )
 }
 
