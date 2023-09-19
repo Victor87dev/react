3886684d9a01d2aa90ba7c74f600fd64e0e54863
Victor87dev <devsjoaovvictor@gmail.com>
2023-09-19 09:52:37 -0300

    trabalhando com eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import React, { useState } from 'react'
-import Header from './componentes/Header'
-import Corpo from './componentes/Corpo'
-import './App.css'
-import Relogio from './componentes/Relogio'
-import Numero from './componentes/Numero'
+import React, {useState} from 'react'
+import Led from './componentes/Led'
+
+
 
 export default function App(){
-const [num,setNum] = useState(20)
-const [nome,setNome] = useState('joao')
-let n1 = 10
 
-const n100 = ()=>{
-n1 = 100
-console.log(n1)
-}
+  const [ligado,setLigado]=useState(true)
 
+  const cancelar =(obj)=>{
+    return obj.preventDefault()
+  }
 
   return(
   <>
-  
-  <p className='texto'>heloow</p>
-  <Header/>
-  <Corpo/>
-  <Relogio/>
-  <p>Valor do state num em App: {num}</p>
-  <Numero num={num} setNum={setNum}/>
-  <p>{nome}</p>
+    <Led ligado={ligado} setLigado={setLigado}/>
+    <a href="https://youtube.com/cfbcursos" target='_blank' onClick={(e)=>cancelar(e)}>cfb cursos</a>
   </>
   
   )
